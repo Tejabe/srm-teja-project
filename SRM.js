79a9292778787963831aa6c8264769a396de31ea
tejash bezawada <tejabe2004@gmail.com>
2024-09-17 10:50:33 +0530

    teja srm

---
+++ SRM.js
@@ -0,0 +1,36 @@
+function init() {
+    const hamburger = document.querySelector(".hamburger");
+const sidebarclosebutton = document.querySelector(".close-button");
+const sidebarcontainer = document.querySelector(".sidebar-container");
+
+const opensidebar = () => {
+    sidebarcontainer.classList.add("show-sidebar");
+};
+
+const closesidebar = () => {
+    sidebarcontainer.classList.remove("show-sidebar");
+}
+
+hamburger.addEventListener("click", opensidebar);
+sidebarclosebutton.addEventListener("click",closesidebar);
+}
+
+init();
+
+function navrelatedcode() {
+    const navbarcontainer = document.querySelector(".nav-container");
+console.log(navbarcontainer);
+
+window.addEventListener("scroll", function name(params) {
+    const scrollnumber = window.scrollY;
+    const targetnumber = window.innerWidth >=992 ? 70: 50;
+    if (scrollnumber >= targetnumber) {
+        navbarcontainer.classList.add("sticky-nav");
+    } else {
+        navbarcontainer.classList.remove("sticky-nav")
+    }
+});
+}
+ 
+navrelatedcode();
+
